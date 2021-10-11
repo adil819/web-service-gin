6e5ba12611e89eca690bf1053dc10d83e005034f
adil <ahmad.adil@xlfutureleaders.com>
2021-10-11 16:40:13 +0700

    feature api get and post available

--- main.go
+++ main.go
@@ -24,11 +24,32 @@ var albums = []album{
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8080")
+
+	// when "go run ." we can type curl in cmd or postman:
+	// GET => curl http://localhost:8080/albums
+	// POST (view lines) => curl http://localhost:8080/albums --include --header "Content-Type: application/json" --request "POST" --data '{"id": "4", "title": "Rudy Habibie", "artist": "Reza Rahardian", "price": 49.99)'
+
 }
 
 // getAlbums responds with the list of all albums as JSON.
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
+}
+
+// postAlbums adds an album from JSON received in the request body.
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	// Call BindJSON to bind the received JSON to
+	// newAlbum.
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	// Add the new album to the slice.
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
